b84d2fa392ec2565a06b57682c558e36322167f4
Geneño Sampayan <64364440+geneno00@users.noreply.github.com>
2023-12-26 17:24:29 +0800

    digital business card solo project

---
+++ digital-business-card/App.js
@@ -0,0 +1,11 @@
+
+
+export default function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
---
+++ digital-business-card/components/Footer.js
@@ -0,0 +1,12 @@
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <img src="../images/twitter.png" alt="twitter"/>
+            <img src="../images/facebook.png" alt="facebook"/>
+            <img src="../images/instagram.png" alt="instragram"/>
+            <img src="../images/linkedin.png" alt="linkedin"/>
+            <img src="../images/github.png" alt="github"/>
+        </footer>
+    )
+}
